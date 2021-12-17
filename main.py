eff3ece29392fd79046bf1e55e5e23e3bb67292a
Ron Dover <rondover@Rons-MacBook-Pro.local>
2021-12-17 15:43:53 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter,
+)
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+resource = Resource(attributes={"service.name": "fastapi-blog"})
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer(__name__)
+
+otlp_exporter = OTLPSpanExporter(
+    endpoint="otel-collector:4317", insecure=True
+)
+
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.set_tracer_provider(TracerProvider())
+
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+jaeger_exporter = JaegerExporter(
+    agent_host_name='localhost',
+    agent_port=6831)
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(jaeger_exporter, max_export_batch_size=10)
+)
+app = FastAPI()
+FastAPIInstrumentor.instrument_app(app)
+
+
+@app.get("/")
+async def root():
+    raise Exception("test exception")
+    return {"message": "Hello World"}
