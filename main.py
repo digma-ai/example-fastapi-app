d5dc7ee15e21ed61f7f77a590eff46e8189b3cb5
Ron Dover <rondover@Rons-MacBook-Pro.local>
2021-12-21 10:03:32 +0200

    updated

--- main.py
+++ main.py
@@ -12,17 +12,22 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 resource = Resource(attributes={"service.name": "fastapi-blog"})
 trace.set_tracer_provider(TracerProvider(resource=resource))
 tracer = trace.get_tracer(__name__)
-trace.set_tracer_provider(TracerProvider())
 
 jaeger_exporter = JaegerExporter(
     agent_host_name='localhost',
     agent_port=6831)
+    
 trace.get_tracer_provider().add_span_processor(
     BatchSpanProcessor(jaeger_exporter, max_export_batch_size=10)
 )
 app = FastAPI()
 FastAPIInstrumentor.instrument_app(app)
 
+
+@app.get("/users")
+async def get_users():
+    raise Exception("test exception")
+    return {"message": "Hello World"}
 
 @app.get("/")
 async def root():
