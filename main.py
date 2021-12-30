a0e8348c39baf2c38dcb72eea0f0330e4e989e4c
Shay Keren <shay.keren@gmail.com>
2021-12-30 13:12:34 +0200

    add environment parameter

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import os
+
+import sys
+import traceback
+
 import uvicorn as uvicorn
 from fastapi import FastAPI
 from opentelemetry import trace
@@ -6,8 +11,14 @@ from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.digma import DigmaExporter
-from user_service import UserService
-from user_store import UserStore
+from user.user_service import UserService
+import git
+from dotenv import load_dotenv
+
+load_dotenv()
+
+repo = git.Repo(search_parent_directories=True)
+os.environ['GIT_COMMIT_ID'] = repo.head.object.hexsha
 
 resource = Resource(attributes={"service.name": "fastapi-blog"})
 trace.set_tracer_provider(TracerProvider(resource=resource))
@@ -31,8 +42,13 @@ async def get_users():
 
 @app.get("/")
 async def root():
-    user_service.all()
-
+    try:
+        #raise_error()
+        user_service.all()
+    except Exception as ex:
+        ex_type, ex, tb = sys.exc_info()
+        ss = traceback.extract_tb(tb)
+        raise ex
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
