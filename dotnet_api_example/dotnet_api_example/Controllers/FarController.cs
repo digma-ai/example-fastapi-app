9d413fb53f86710d70a416b2ed3ba4468ebe9a44
Noam Kfir <noam@digma.ai>
2022-04-13 15:20:26 +0300

    Add several controllers with different ways of creating ActivitySources

---
+++ dotnet_api_example/dotnet_api_example/Controllers/FarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using dotnet_api_example.Utils;
+
+namespace dotnet_api_example.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FarController : ControllerBase
+{
+    public FarController()
+    {
+    }
+
+    [HttpGet(Name = "far")]
+    public string Get()
+    {
+        using (var activity = ActivityUtil.Activity.StartActivity("Far Route", ActivityKind.Producer)) {
+            return "FAR OUT!!!";
+        }
+    }
+}
