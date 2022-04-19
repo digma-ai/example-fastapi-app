eca68cdff4e8890d83af26084c4fbc3cc4fc11d8
Shay Keren <shay.keren@gmail.com>
2022-04-19 19:17:35 +0300

    go example changes

--- go/user-service/user/controller.go
+++ go/user-service/user/controller.go
@@ -2,42 +2,68 @@ package user
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"go.opentelemetry.io/otel/attribute"
+	otelcodes "go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type UserController struct {
-	Service Service
+	service Service
+	tracer  trace.Tracer
+}
+
+func NewUserController(service Service, tracer trace.Tracer) UserController {
+	return UserController{
+		service: service,
+		tracer:  tracer,
+	}
 }
 
 func (controller *UserController) Get(w http.ResponseWriter, req *http.Request) {
-	userId := mux.Vars(req)["id"]
-	user, _ := controller.Service.Get(userId)
-	json.NewEncoder(w).Encode(user)
+	_, span := controller.tracer.Start(req.Context(), "controller::Get")
+	defer span.End()
+
+	userId := req.URL.Query().Get("id")
+	user, _ := controller.service.Get(userId)
+	setResponse(w, user, http.StatusOK)
 }
 
 func (controller *UserController) Add(w http.ResponseWriter, req *http.Request) {
+	_, span := controller.tracer.Start(req.Context(), "controller::Add")
+	defer span.End()
 	var user User
-	reqBody, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		fmt.Fprintf(w, "please enter user details")
+
+	if err := json.NewDecoder(req.Body).Decode(&user); err != nil {
+		span.SetStatus(otelcodes.Error, "request decode error")
+		span.RecordError(err, trace.WithStackTrace(true))
+		setResponse(w, err, http.StatusBadRequest)
+		return
 	}
-	json.Unmarshal(reqBody, &user)
-	error := controller.Service.Add(user)
+
+	error := controller.service.Add(user)
 	if error != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(error.Error())
+		span.RecordError(error, trace.WithStackTrace(true))
+		setResponse(w, error.Error(), http.StatusBadRequest)
 	} else {
-		w.WriteHeader(http.StatusCreated)
-		json.NewEncoder(w).Encode(user)
+		span.AddEvent("user", trace.WithAttributes(attribute.String("Id", user.Id)))
+		span.SetStatus(otelcodes.Ok, "user added")
+		span.SetAttributes(attribute.String("Test", "Test"))
+		setResponse(w, user, http.StatusCreated)
 	}
 
 }
 
 func (controller *UserController) All(w http.ResponseWriter, req *http.Request) {
-	users, _ := controller.Service.List()
-	json.NewEncoder(w).Encode(users)
+	_, span := controller.tracer.Start(req.Context(), "controller::All")
+	defer span.End()
+	users, _ := controller.service.List()
+	setResponse(w, users, http.StatusOK)
+}
+
+func setResponse(w http.ResponseWriter, output interface{}, statusCode int) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	return json.NewEncoder(w).Encode(output)
 }
